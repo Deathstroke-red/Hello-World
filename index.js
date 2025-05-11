733606f2c4f70fad56f0a95edd5c4d67638863af
Deathstroke-red~ <normanbishepard@gmail.com>
2025-05-11 16:25:33 +1000

    Добавить файл для module_8

--- index.js
+++ index.js
@@ -1,90 +1,74 @@
-// Задание 7.6.1
+// // Задание 8.7.1.
 
-// const word = prompt("Введите слово для проверки:");
-// if (word === null || word.trim() === "") {
-//     console.log("Вы ничего не ввели!");
-// } else {
-//     const cleanWord = word.toLowerCase().replace(/\s/g, '');
-    
-//     const reversedWord = cleanWord.split('').reverse().join('');
-    
-//     if (cleanWord === reversedWord) {
-//         console.log(`Слово "${word}" является палиндромом`);
-//     } else {
-//         console.log(`Слово "${word}" не является палиндромом`);
-//     }
-// }
-
-
-
-// Задание 7.6.2.
-
-// const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know'];
-// const uniqueArr = [...new Set(arr)];
-// console.log(uniqueArr); //в консоли браузереа или через node.js должен быть ответ [1, 2, 3, 5, 4, 'they', 'don\'t', 'know', 'that', 'we'];
-
-
-// Задание 7.6.3.
-
-// const userInput = prompt("Введите любое число:");
-
-// if (userInput === null) {
-//   console.log("отменили ввод!");
-// } else {
-//   const number = Number(userInput);
-  
-//   if (isNaN(number)) {
-//     console.log("ввели не число!");
-//   } else {
-//     const numbersArray = [];
-//     for (let i = 0; i <= number; i++) {
-//       numbersArray.push(i);
-//     }
-    
-//     console.log(`Массив чисел от 0 до ${number}:`, numbersArray);
+// function printInfo() {
+//     console.log(`Name: ${this.name}, Age: ${this.age}`);
 //   }
-// }
+//   const person = {
+//     name: 'Иван',
+//     age: 30
+//   };
+//   printInfo.call(person);
 
-// Задание 7.6.4.
+// // Задание 8.7.2.
 
-// const board = [
-//     ['x', 'o', 'x'],
-//     ['o', 'x', 'o'],
-//     ['x', 'o', 'x']
+// function calculate(a, b, operator) {
+//     switch (operator) {
+//       case '+':
+//         return a + b;
+//       case '-':
+//         return a - b;
+//       case '*':
+//         return a * b;
+//       case '/':
+//         return a / b;
+//     }
+//   }
+//   const params = {
+//     a: 2,
+//     b: 3,
+//     operator: '+'
+//   };
+//   const result = calculate.apply(null, [params.a, params.b, params.operator]);
+//   console.log(result);
+
+// // Задание 8.7.3.
+
+// const users = [
+//     { name: 'Таня', age: 23 },
+//     { name: 'Игорь', age: 16 },
+//     { name: 'Людмила', age: 26 },
+//     { name: 'Андрей', age: 13 },
+//     { name: 'Елена', age: 21 }
 //   ];
   
-//   for (let i = 0; i < 3; i++) {
-//     let row = '';
-//     for (let j = 0; j < 3; j++) {
-//       row += board[i][j];
-//       if (j < 2) {
-//         row += ' ';
-//       }
-//     }
-//     console.log(row);
+//   const adults = users.filter(user => user.age >= 18);
+//   console.log('Совершеннолетние пользователи:', adults);
+  
+//   const names = adults.map(user => user.name);
+//   console.log('Имена совершеннолетних пользователей:', names);
+
+// // Задание 8.7.4.
+
+// function setFullName(fullName) {
+//     this.fullName = fullName;
 //   }
+//   const person = {
+//     name: 'Джон',
+//     age: 33
+//   };
+//   const setPersonFullName = setFullName.bind(person);
+//   setPersonFullName("John Smith");
+//   console.log(person);
+
+// Задание 8.7.5.
 
 
-// Задание 7.6.5.
-
-
-const obj = {
-    some: 'some',
-    dom: 'text',
-    arr: [1, 2, 3, 4, 5],
-    tom: 'there'
-};
-
-const arrValues = [];
-
-for (let key in obj) {
-    const value = obj[key];
+function getUniqueSortedNumbers(numbers) {
+    const uniqueNumbers = [...new Set(numbers)];
+    const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
     
-    if (Array.isArray(value)) {
-        arrValues.push(...value);
-    } else {
-        arrValues.push(value);
-    }
-}
-
-console.log(arrValues);
+    return sortedNumbers;
+  }
+  const numbers = [6, 8, 4, 7, 5, 1, 2, 9, 5, 7, 1];
+  const result = getUniqueSortedNumbers(numbers);
+  console.log(result);
